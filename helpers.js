8f73a0d1f11de922d56ad70d4c8966e8db8dfe6a
Mark Obuobi <mark.eo@kraftkey.com>
2022-09-10 13:23:21 +0000

    Minor updates

--- helpers.js
+++ helpers.js
@@ -1,1 +1,2 @@
 export const apiUrl = 'https://flutter-messages.kraftkey.com/api'
+// export const apiUrl = 'http://localhost:8000/api'
--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,27 @@ export default function Home(){
             .then(res => res.json())
             .then(data => {
                 setMessages(data)
+                let scroller = document.querySelector('.messages-list')
+                    scroller.scrollTop = scroller.scrollHeight
             })
+
     }
 
     function sendMessage(e){
         e.preventDefault()
+        setMessage('')
+
+        //display message before sending with a blue bubble to denote sending
+        let previewMsg = {
+            sender:sender,
+            message:message,
+            status:'preview'
+        }
+
+        messages.push(previewMsg)
+
+        setMessages([...messages])
+
         fetch(`${apiUrl}/message/send`,{
             method:'POST',
             body:JSON.stringify({
@@ -31,7 +47,6 @@ export default function Home(){
             .then(res => res.json())
             .then(() => {
                 getMessages()
-                setMessage('')
             })
     }
 
@@ -45,22 +60,20 @@ export default function Home(){
               <title>Flutter Messages</title>
           </Head>
           <div className="w-1/2 mx-auto my-5">
-              <form onSubmit={sendMessage}>
                   <div className="flex flex-col gap-3">
                       <input type="text" placeholder="Your Name" className="border border-blue-400 rounded py-2 px-4" onChange={(e)=>{setSender(e.target.value)}} value={sender} autoFocus={true}/>
-                      <textarea placeholder="Your Message" className="border border-blue-400 rounded py-2 px-4" value={message} defaultValue={message} onChange={(e)=>{setMessage(e.target.value)}}/>
-                      <button className="bg-green-400 py-3 rounded text-white">Send</button>
+                      <textarea placeholder="Your Message" className="border border-blue-400 rounded py-2 px-4" onChange={(e)=>{setMessage(e.target.value)}} value={message}/>
+                      <button className="bg-green-400 py-3 rounded text-white" onClick={sendMessage}>Send</button>
                   </div>
-              </form>
               <hr className="border-blue-100 my-5"/>
               <div className="text-center">
                   <button onClick={getMessages}>Refresh</button>
               </div>
-              <div>
+              <div className="bg-red-300 h-[300px] overflow-scroll messages-list">
                   {messages.length > 0 &&
                       <>
                           {messages.map(message => (
-                              <div key={message.id} className="w-8/12 bg-blue-200 text-black px-4 pb-2 m-2 float-right rounded-3xl">
+                              <div key={message.id} className={`${message.status ? 'bg-gray-200' : 'bg-green-200'} w-8/12 text-black px-4 pb-2 m-2 float-right rounded-3xl`}>
                                   <div>
                                       <small className="text-gray-600">{message.sender}</small>
                                   </div>
